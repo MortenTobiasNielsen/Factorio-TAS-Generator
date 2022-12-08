c9787e33c08e485cbb6ca99b27c63f31f1ea0ea2
Theis <theis99999@gmail.com>
2022-12-08 12:28:47 +0100

    Add list of task names, to convert from task type to task name

--- src/TaskNameToEnum.h
+++ src/TaskNameToEnum.h
@@ -2,9 +2,11 @@
 
 #include <map>
 #include <string>
+#include <vector>
 
 using std::map;
 using std::string;
+using std::vector;
 
 /// <summary>
 /// Enumaration of all task types
@@ -14,7 +16,19 @@ enum TaskType
 	e_start = 1, e_stop, e_build, e_craft, e_game_speed, e_pause, e_save, e_recipe, e_limit, e_filter, e_rotate, e_priority, e_put, e_take, e_mine, e_launch, e_walk, e_tech, e_drop, e_pick_up, e_idle
 };
 
-static const map<string, TaskName> TaskNames = {{"Start", e_start}, {"Stop", e_stop}, {"Build", e_build}, {"Craft", e_craft}, {"Game Speed", e_game_speed}, {"Pause", e_pause}, {"Save", e_save},
+/// <summary>
+/// Contains every task name, aligned with TaskType
+/// </summary>
+/// <note>
+/// Starts with "None" for alignments
+/// </note>
+static const vector<string> TaskNames{
+	"None", 
+	"Start", "Stop", "Build", "Craft", "Game Speed", "Pause", "Save",
+	"Recipe", "Limit", "Filter", "Rotate", "Priority", "Put", "Take", "Mine", "Launch",
+	"Walk", "Tech", "Drop", "Pick up", "Idle"
+};
+
 /// <summary>
 /// Maps TaskName(string) to TaskType(enum)
 /// </summary>
