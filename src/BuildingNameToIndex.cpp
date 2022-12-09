196896c6c69026fb2ecacac934848f2aada00a18
Theis <theis99999@gmail.com>
2022-12-09 10:15:43 +0100

    Formatting

--- src/BuildingNameToIndex.cpp
+++ src/BuildingNameToIndex.cpp
@@ -2,6 +2,12 @@
 
 string FindBuildingName(int index)
 {
-	if (index < 1 || index > BuildingNames.size()) return "N/A";
-	else return BuildingNames[index];
+	if (index < 1 || index > BuildingNames.size())
+	{
+		return "N/A";
+	}
+	else
+	{
+		return BuildingNames[index];
+	}	
 }
