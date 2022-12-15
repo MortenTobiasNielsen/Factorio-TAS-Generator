71c2279340759b2daf4763a645e26e3e2000532b
Theis <theis99999@gmail.com>
2022-12-15 12:34:01 +0100

    Added rightclick handler to add task button. Which adds a task at the end of a selected area.

--- src/cMain.h
+++ src/cMain.h
@@ -114,6 +114,7 @@ protected:
 
 	// Step
 	void OnAddStepClicked(wxCommandEvent& event);
+	void OnAddStepRightClicked(wxMouseEvent & event);
 	void OnChangeStepClicked(wxCommandEvent& event);
 	void OnDeleteStepClicked(wxCommandEvent& event);
 	void OnMoveUpClicked(wxCommandEvent& event);
