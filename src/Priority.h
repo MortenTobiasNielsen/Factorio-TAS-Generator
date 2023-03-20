0a5f897ad541dfc7f3667413634097e6dbb59738
Theis <theis99999@gmail.com>
2023-03-20 11:15:26 +0100

    Added Orientation.h and Priority.h Moved orientation and direction to Orientation.h Moved priority to Priority.h Removed struct_direction_list as it was a duplicate of build_orientations

---
+++ src/Priority.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <vector>
+#include <map>
+#include <string>
+
+enum INPUT_OUTPUT
+{
+	LEFT, NONE, RIGHT
+};
+
+static const std::vector<std::string> input_output =
+{
+	"Left",
+	"None",
+	"Right"
+};
+
+static inline std::map<std::string, INPUT_OUTPUT> map_input_output =
+{
+	{input_output[0], LEFT},
+	{input_output[1], NONE},
+	{input_output[2], RIGHT}
+};
