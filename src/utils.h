b0f37351e54453158f730ecf673f757a5fc2fa95
Theis <theis99999@gmail.com>
2023-03-20 11:34:33 +0100

    Added Inventory.h and moved appropiate constants to it

--- src/utils.h
+++ src/utils.h
@@ -6,6 +6,7 @@
 
 #include "Priority.h"
 #include "Orientation.h"
+#include "Inventory.h"
 
 using std::string;
 using std::vector;
@@ -893,16 +894,6 @@ static const std::vector<std::string> item_combat =
 	"Shotgun"
 };
 
-static const std::vector<std::string> take_from = 
-{
-	"Input",
-	"Output",
-	"Fuel",
-	"Modules",
-	"Chest",
-	"Wreck"
-};
-
 static const std::vector<std::string> module_list = 
 {
 	"Speed module",
@@ -1116,16 +1107,6 @@ static const std::vector<std::string> drills_list =
 
 static const struct
 {
-	std::string input = "Input";
-	std::string output = "Output";
-	std::string fuel = "Fuel";
-	std::string modules = "Modules";
-	std::string chest = "Chest";
-	std::string wreck = "Wreck";
-} struct_from_into_list;
-
-static const struct
-{
 	std::string stone = "Stone furnace";
 	std::string steel = "Steel furnace";
 } struct_auto_put_furnace_list;
@@ -1158,16 +1139,3 @@ static const struct
 	std::string yellow_science = "utility-science-pack";
 	std::string white_science = "space-science-pack";
 } struct_science_list;
-
-static const struct
-{
-	std::string chest = "defines.inventory.chest";
-	std::string lab_input = "defines.inventory.lab_input";
-	std::string lab_modules = "defines.inventory.lab_modules";
-	std::string beacon_modules = "defines.inventory.beacon_modules";
-	std::string fuel = "defines.inventory.fuel";
-	std::string drill_modules = "defines.inventory.mining_drill_modules";
-	std::string assembly_input = "defines.inventory.assembling_machine_input";
-	std::string assembly_output = "defines.inventory.assembling_machine_output";
-	std::string assembly_modules = "defines.inventory.assembling_machine_modules";
-} struct_take_put_list;
